9a1d28b9c909600ea845f7e66c2e98b03622f248
tamer <tamer@DESKTOP-SSRC7SB.localdomain>
2022-11-26 12:38:34 -0500

    testing

--- bin/cli.js
+++ bin/cli.js
@@ -2,4 +2,4 @@
 import minimist from "minimist"
 import rolls from "../lib/roll.js"
 var args= minimist(process.argv.slice(2));
-console.log(rolls(args.side, args.dice, args.rolls));
+console.log(rolls(args.sides, args.dice, args.rolls));
