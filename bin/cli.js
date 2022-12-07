5c40348c6c887cee08db723f8f24caa7fe2a1d63
tamer <tamer@DESKTOP-SSRC7SB.localdomain>
2022-12-07 13:08:35 -0500

    fixing import

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import minimist from "minimist"
-import rolls from "../lib/roll.js"
+import roll from "../lib/roll.js"
 var args= minimist(process.argv.slice(2));
 console.log(rolls(args.sides, args.dice, args.rolls));
